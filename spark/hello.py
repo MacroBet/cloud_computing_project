3c33a985d35543ded9510a26be13c8870899d1b0
Marco Bellia <ing.bellia@gmail.com>
2022-05-30 16:01:33 +0200

    test bloom filter creation

--- spark/hello.py
+++ spark/hello.py
@@ -1,7 +1,10 @@
 import sys
 from operator import add
+from bloomfilter import BloomFilter
+
 
 from pyspark import SparkContext
+p = 0.1 #false positive probability
 
 
 def count_ratings_occurences(file_name):
@@ -15,6 +18,12 @@ def count_ratings_occurences(file_name):
     else:
         output = counts.collect()
         return output
+
+def insert_ratings_in_bloom_filters(file_name,bloomFilters):
+    lines = sc.textFile(file_name)
+    ratings = lines.flatMap(lambda x:( x.split('\t')[0],round(float(x.split('\t')[1]))))
+    output = ratings.map(lambda rating: bloomFilters[rating[1]-1].add(rating[0]))
+    return output.collect()
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -25,8 +34,16 @@ if __name__ == "__main__":
     sc = SparkContext(master, "WordCount")
 
     rating_count= count_ratings_occurences(sys.argv[1])
+    #   1,2,3,4,5,6,7,8,9,10
+    N= [0,0,0,0,0,0,0,0,0,0]
+
     for (word, count) in rating_count:
+        N[int(word)-1]=count
         print("%s: %i" % (word, count))
 
+    total_elements= sum(N)
+    bloomFilters = [BloomFilter(N[i],p) for i in range(len(N))]
+    print("HO creato i miei bei bloom filters")
+    output = insert_ratings_in_bloom_filters(sys.argv[1],bloomFilters)
 
     
