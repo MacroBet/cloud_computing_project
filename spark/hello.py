354632be9f6fa7d4871bbfbfee31ace535c3b6a5
Marco Bellia <ing.bellia@gmail.com>
2022-05-30 15:30:35 +0200

    test

--- spark/hello.py
+++ spark/hello.py
@@ -11,10 +11,17 @@ if __name__ == "__main__":
     sc = SparkContext(master, "WordCount")
 
     lines = sc.textFile(sys.argv[1])
-    ratings = lines.flatMap(lambda x: [x.split('\t')[0],round(float(x.split('\t')[1]))])
 
+# ["id1 5 10","id2 7 10","id3 8 10"] => ["id1","5","10"..]
+# 
+# 
+
+    ratings= lines.map(lambda x: (x.split('\t')[0],round(float(x.split('\t')[1]))))
+    # ratings = lines.flatMap(lambda x: [x.split('\t')[0],round(float(x.split('\t')[1]))])
     # words = lines.flatMap(lambda x: x.split(' '))
-    ones =  ratings.map(lambda x: (x[1], 1))
+    ratingToCount= ratings.flatMap(lambda x: x[1])
+
+    ones =  ratingToCount.map(lambda x: (x, 1))
     counts = ones.reduceByKey(add)
 
     if len(sys.argv) == 3:
