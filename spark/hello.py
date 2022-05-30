8fc3ab1d173507058167072dfac7da5865f106e1
Marco Bellia <ing.bellia@gmail.com>
2022-05-30 16:33:59 +0200

    debug

--- spark/hello.py
+++ spark/hello.py
@@ -22,7 +22,7 @@ def count_ratings_occurences(file_name):
 def insert_ratings_in_bloom_filters(file_name,bloomFilters):
     lines = sc.textFile(file_name)
     ratings = lines.map(lambda x: ( x.split('\t')[0],round(float(x.split('\t')[1]))))
-    ratings.map(lambda rating: bloomFilters[rating[1]-1].add(rating[0]))
+    ratings.map(lambda rating: print(rating) )#bloomFilters[rating[1]-1].add(rating[0])
     ratings.collect()
     
 if __name__ == "__main__":
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     total_elements= sum(N)
     bloomFilters = [BloomFilter(N[i],p,"Rate "+ str(i+1)) for i in range(len(N))]
     print("HO creato i miei bei bloom filters")
-    output = insert_ratings_in_bloom_filters(sys.argv[1],bloomFilters) 
+    insert_ratings_in_bloom_filters(sys.argv[1],bloomFilters) 
     print("funziona? "+ str(bloomFilters[5].check("tt0000001")))
 
     
