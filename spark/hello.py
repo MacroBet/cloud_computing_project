51e7a9eff892e6d3495c006e113d090abf1548e2
Marco Bellia <ing.bellia@gmail.com>
2022-05-30 15:38:56 +0200

    test

--- spark/hello.py
+++ spark/hello.py
@@ -1,5 +1,6 @@
 import sys
 from operator import add
+
 from pyspark import SparkContext
 
 if __name__ == "__main__":
@@ -11,32 +12,13 @@ if __name__ == "__main__":
     sc = SparkContext(master, "WordCount")
 
     lines = sc.textFile(sys.argv[1])
+    words = lines.flatMap(lambda x: round(x.split('/t')[1]))
+    ones =  words.map(lambda x: (x, 1))
+    counts = ones.reduceByKey(add)
 
-# ["id1 5 10","id2 7 10","id3 8 10"] => ["id1","5","10"..]
-# 
-# 
-
-
-    ratings= []
-    for line in lines:
-        ratings.append([line.split('\t')[0],round(float(line.split('\t')[1]))])
-    
-    print("Output:")
-    print(ratings[0])
-    
-    # ratings = lines.flatMap(lambda x: [x.split('\t')[0],round(float(x.split('\t')[1]))])
-    # words = lines.flatMap(lambda x: x.split(' '))
-
-
-
-    # ratingToCount= ratings.flatMap(lambda x: x[1])
-
-    # ones =  ratingToCount.map(lambda x: (x, 1))
-    # counts = ones.reduceByKey(add)
-
-    # if len(sys.argv) == 3:
-    #     counts.repartition(1).saveAsTextFile(sys.argv[2])
-    # else:
-    #     output = counts.collect()
-    #     for (word, count) in output:
-    #         print("%s: %i" % (word, count))
+    if len(sys.argv) == 3:
+        counts.repartition(1).saveAsTextFile(sys.argv[2])
+    else:
+        output = counts.collect()
+        for (word, count) in output:
+            print("%s: %i" % (word, count))
