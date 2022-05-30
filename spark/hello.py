00652f8bcd0100683157ad5dd5f58c43bf5a3bcf
Marco Bellia <ing.bellia@gmail.com>
2022-05-30 16:13:46 +0200

    cast bool

--- spark/hello.py
+++ spark/hello.py
@@ -45,8 +45,7 @@ if __name__ == "__main__":
     bloomFilters = [BloomFilter(N[i],p) for i in range(len(N))]
     print("HO creato i miei bei bloom filters")
     output = insert_ratings_in_bloom_filters(sys.argv[1],bloomFilters) 
-    print(output)
 
-    print("funziona? "+bloomFilters[5].check("tt0000001"))
+    print("funziona? "+ str(bloomFilters[5].check("tt0000001")))
 
     
