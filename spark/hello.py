f3c104d729b27341337b721998287fe505e3727c
Marco Bellia <ing.bellia@gmail.com>
2022-05-30 15:49:15 +0200

    code improvement

--- spark/hello.py
+++ spark/hello.py
@@ -3,15 +3,9 @@ from operator import add
 
 from pyspark import SparkContext
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: wordcount <input file> [<output file>]", file=sys.stderr)
-        sys.exit(-1)
 
-    master = "local"
-    sc = SparkContext(master, "WordCount")
-
-    lines = sc.textFile(sys.argv[1])
+def count_ratings_occurences(file_name):
+    lines = sc.textFile(file_name)
     words = lines.flatMap(lambda x: str(round(float(x.split('\t')[1]))))
     ones =  words.map(lambda x: (x, 1))
     counts = ones.reduceByKey(add)
@@ -20,5 +14,19 @@ if __name__ == "__main__":
         counts.repartition(1).saveAsTextFile(sys.argv[2])
     else:
         output = counts.collect()
-        for (word, count) in output:
-            print("%s: %i" % (word, count))
+        return output
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: wordcount <input file> [<output file>]", file=sys.stderr)
+        sys.exit(-1)
+
+    master = "local"
+    sc = SparkContext(master, "WordCount")
+
+    rating_count= count_ratings_occurences(sys.argv[1])
+    for (word, count) in rating_count:
+        print("%s: %i" % (word, count))
+
+
+    
