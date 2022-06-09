0218deb88aff3211bc9f0a5d062b23d036bc4da3
MarcoRalli <marco_ralli@outlook.com>
2022-06-09 11:12:10 +0200

    new implementation

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/BloomFilter.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/BloomFilter.java
@@ -1,9 +1,11 @@
 package it.unipi.hadoop;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.nio.ByteBuffer;
 import java.util.BitSet;
+import java.util.HashMap;
 import java.util.ArrayList;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -21,9 +23,22 @@ import org.apache.hadoop.fs.FileSystem;
 import java.io.IOException;
 import java.io.OutputStream;
 import it.unipi.hadoop.BloomFilterCreator;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
 
 
 public class BloomFilter {
+
+  private static final float p_rate = (float) 0.01;
+
+  public static int get_size(int n, float p) {
+    return (int) (-(n * Math.log(p)) / Math.pow((Math.log(2)), 2));
+  }
+
+  public static int get_hash_count(int size, int n) {
+    return (int) ((size / n) * Math.log(2));
+  }
+
   public static class RatingMapper
       extends Mapper<Object, Text, Text, IntWritable> {
     private Text word = new Text();
@@ -44,8 +59,8 @@ public class BloomFilter {
   }
 
   public static class CreateBloomFilterReducer
-      extends Reducer<Text, IntWritable, Text, IntWritable> {
-    private IntWritable result = new IntWritable();
+      extends Reducer<Text, IntWritable, Text, Text> {
+    private Text result = new Text();
 
     public void reduce(Text key, Iterable<IntWritable> values,
         Context context) throws IOException, InterruptedException {
@@ -55,17 +70,48 @@ public class BloomFilter {
         sum += val.get();
         // ratings.add(val.toString());
       }
-      // BloomFilterCreator bloomFilter= new BloomFilterCreator(sum);
-      // for (String rating : ratings) {
-      //   bloomFilter.add(rating);
-      // }
-      
-      // result.set(bloomFilter.toString());
-      // context.write(key, result); 
-      result.set(sum);
-      context.write(key, result); 
+      int m = get_size(sum, p_rate);
+      int k = get_hash_count(m, sum);
+      Text res = new Text(Integer.toString(m) + " " + Integer.toString(k));
+      result.set(res);
+      context.write(key, res); // output (ratings m k )
     }
   }
+/* 
+  public static class BloomFilterMapper
+  extends Mapper<Object, Text, Text, Text> {
+private Text word = new Text();
+private HashMap<String, ArrayList<Integer>> bloomFilter_param = new HashMap<String, ArrayList<Integer>>();
+
+public void setup(Context context) throws IOException, InterruptedException
+{
+  try{
+    Path pt=new Path("hdfs:/path/to/file");//Location of file in HDFS
+    FileSystem fs = FileSystem.get(new Configuration());
+    BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(pt)));
+    String line;
+    ArrayList<Integer> parameters = new ArrayList<>();
+    line=br.readLine();
+    while (line != null){
+        String[] currencies = line.split(" ");
+        parameters.add(Integer.parseInt(currencies[1]), Integer.parseInt(currencies[2]));
+        bloomFilter_param.put(currencies[0], parameters);
+        System.out.println(line);
+        line=br.readLine();
+
+    }
+}catch(Exception e) {e.getStackTrace();}{
+}
+}
+
+
+public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+ 
+    context.write(word, bloomFilter);
+  }
+}
+} */
+
 
   public static void main(String[] args) throws Exception {
     Configuration conf1 = new Configuration();
@@ -86,7 +132,7 @@ public class BloomFilter {
     job1.setReducerClass(CreateBloomFilterReducer.class);
     // job1.setMapOutputKeyClass(theClass); // set output values for mapper
     job1.setOutputKeyClass(Text.class);
-    job1.setOutputValueClass(IntWritable.class);
+    job1.setOutputValueClass(Text.class);
     
     // for (int i = 0; i < otherArgs.length - 1; ++i) {
     //   FileInputFormat.addInputPath(job1, new Path(otherArgs[i]));
