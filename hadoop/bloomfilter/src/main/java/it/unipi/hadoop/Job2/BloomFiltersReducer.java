1287ab824242426016bf622a66e68ae83fecee88
MarcoRalli <marco_ralli@outlook.com>
2022-06-17 16:24:15 +0200

    test

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job2/BloomFiltersReducer.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job2/BloomFiltersReducer.java
@@ -10,15 +10,19 @@ public class BloomFiltersReducer extends Reducer<Text, BloomFilter, Text, Text> 
 
     public void reduce(Text key, Iterable<BloomFilter> values, Context context) throws IOException, InterruptedException {
             
-        BloomFilter temp_BloomFilter = new BloomFilter(Iterables.get(values, 0).get_size(), 
-                                                                Iterables.get(values, 0).get_hash_count());
+       //BloomFilter temp_BloomFilter = new BloomFilter(Iterables.get(values, 0).get_size(), 
+         //                                                       Iterables.get(values, 0).get_hash_count());
         for (BloomFilter bloomFilter : values) {
+            
 
-            temp_BloomFilter.get_bitSet().or(bloomFilter.get_bitSet());
+            context.write(key, new Text(bloomFilter.get_bitSet().toString()));
+           // temp_BloomFilter.get_bitSet().or(bloomFilter.get_bitSet());
         }
 
-        context.write(key, new Text(temp_BloomFilter.toString()));
+        //context.write(key, new Text(temp_BloomFilter.toString()));
     
       }
+
+
      
 }
