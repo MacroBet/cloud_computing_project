683e277ed134f80dbffb9ddcc764ecc87a7dfb1e
MarcoRalli <marco_ralli@outlook.com>
2022-06-17 15:42:18 +0200

    fix2

--- hadoop/bloomfilter/deploy.sh
+++ hadoop/bloomfilter/deploy.sh
@@ -1,7 +1,8 @@
 git pull
 hadoop fs -rm -r output
+hadoop fs -rm -r output_2
 mvn clean package
 cd target
-hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.BloomFilter mini_ratings.txt output
+hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main mini_ratings.txt output
 hadoop fs -cat output/part-r-00000
 hadoop fs -cat output_2/part-r-00000
