9dd216e276281cb2c948ac146228cd2c4e5b3ed6
Giulio <fischiettigiulio@gmail.com>
2022-06-20 17:45:18 +0200

    bytes add

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/BloomFilter.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/BloomFilter.java
@@ -1,4 +1,5 @@
 package it.unipi.hadoop;
+
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.VIntWritable;
 import java.util.BitSet;
@@ -8,7 +9,7 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class BloomFilter implements Writable{
+public class BloomFilter implements Writable {
   private VIntWritable size;
   private VIntWritable hashCount;
   private BytesWritable bytes;
@@ -22,7 +23,6 @@ public class BloomFilter implements Writable{
     return (int) ((size / n) * Math.log(2));
   }
 
-  
   public BloomFilter() {
     bytes = new BytesWritable();
     size = new VIntWritable();
@@ -31,7 +31,7 @@ public class BloomFilter implements Writable{
 
   BloomFilter(int n) {
     int size = get_size(n, (float) 0.01);
-    int hashCount= get_hash_count(size, n);
+    int hashCount = get_hash_count(size, n);
     this.size = new VIntWritable(size);
     this.hashCount = new VIntWritable(hashCount);
     this.bitset = new BitSet(size);
@@ -44,21 +44,22 @@ public class BloomFilter implements Writable{
     this.hashCount = new VIntWritable(b.get_hash_count());
     this.bitset = b.get_bitset();
     this.bytes = b.bytes;
-    
+
   }
 
   // BloomFilter(int size, int hashCount, String bitSet) {
-  //   this.size = size;
-  //   this.hashCount = hashCount;
-  //   // short a = Short.parseShort(bitSet, 2);
-  //   // ByteBuffer bytes = ByteBuffer.allocate((int) Math.round(size / 8 +
-  //   // 0.5)).putShort(a);
-  //   // byte[] array = bytes.array();
-  //   this.bitSet = fromString(bitSet);
+  // this.size = size;
+  // this.hashCount = hashCount;
+  // // short a = Short.parseShort(bitSet, 2);
+  // // ByteBuffer bytes = ByteBuffer.allocate((int) Math.round(size / 8 +
+  // // 0.5)).putShort(a);
+  // // byte[] array = bytes.array();
+  // this.bitSet = fromString(bitSet);
   // }
 
   @Override
   public void write(DataOutput dataOutput) throws IOException {
+
     bytes.write(dataOutput);
     size.write(dataOutput);
     hashCount.write(dataOutput);
@@ -69,9 +70,8 @@ public class BloomFilter implements Writable{
     bytes.readFields(dataInput);
     size.readFields(dataInput);
     hashCount.readFields(dataInput);
-    this.bitset= BitSet.valueOf(bytes.getBytes());
+    this.bitset = BitSet.valueOf(bytes.getBytes());
   }
-
 
   public BloomFilter(int size, int hashCount) {
     this.size = new VIntWritable(size);
@@ -80,7 +80,7 @@ public class BloomFilter implements Writable{
     this.bytes = new BytesWritable(bitset.toByteArray());
   }
 
-  public void or(BloomFilter bloomFilter){
+  public void or(BloomFilter bloomFilter) {
     this.bitset.or(bloomFilter.get_bitset());
   }
 
@@ -89,7 +89,7 @@ public class BloomFilter implements Writable{
   }
 
   public void add(String s) {
-    int hashCount= this.hashCount.get();
+    int hashCount = this.hashCount.get();
     int size = this.size.get();
     for (int i = 0; i < hashCount; i++) {
 
@@ -99,11 +99,13 @@ public class BloomFilter implements Writable{
       // digest += this.size;
       // }
       this.bitset.set(digest);
+
     }
+    this.bytes = new BytesWritable(bitset.toByteArray());
   }
 
   public boolean check(String s) {
-    int hashCount= this.hashCount.get();
+    int hashCount = this.hashCount.get();
     BitSet bitset = BitSet.valueOf(this.bytes.getBytes());
     int size = this.size.get();
     for (int i = 0; i < hashCount; i++) {
@@ -120,38 +122,49 @@ public class BloomFilter implements Writable{
 
   // @Override
   // public String toString() {
-  //   return this.size + "\t" + this.hashCount + "\t" + toString(this.bitSet);
+  // return this.size + "\t" + this.hashCount + "\t" + toString(this.bitSet);
 
   // }
 
   // private static BitSet fromString(final String str_bitset) {
-  //   BitSet bitSet = new BitSet(str_bitset.length());
-  //   // System.out.println(str_bitset);
+  // BitSet bitSet = new BitSet(str_bitset.length());
+  // // System.out.println(str_bitset);
 
-  //   for (int i = 0; i < str_bitset.toCharArray().length; i++) {
-  //     bitSet.set(str_bitset.toCharArray().length - 1 - i, ((str_bitset.charAt(i)) == '1' ? true : false));
-  //   }
-  //   return bitSet;
+  // for (int i = 0; i < str_bitset.toCharArray().length; i++) {
+  // bitSet.set(str_bitset.toCharArray().length - 1 - i, ((str_bitset.charAt(i))
+  // == '1' ? true : false));
+  // }
+  // return bitSet;
   // }
 
   // private static String toString(BitSet bs) {
-  //   String strlong = "";
+  // String strlong = "";
 
-  //   for (int i = 0; i < bs.toLongArray().length; i++) {
-  //     int n_fill = 64 - Long.toString(bs.toLongArray()[bs.toLongArray().length - 1 - i], 2).length();
-  //     String fill_bits = new String(new char[n_fill]).replace('\0', '0');
-  //     strlong = strlong + fill_bits + Long.toString(bs.toLongArray()[bs.toLongArray().length - 1 - i], 2);
-  //   }
-  //   System.out.println("STRINGA BIT LUNGA = " + strlong.length());
-  //   return strlong;
+  // for (int i = 0; i < bs.toLongArray().length; i++) {
+  // int n_fill = 64 - Long.toString(bs.toLongArray()[bs.toLongArray().length - 1
+  // - i], 2).length();
+  // String fill_bits = new String(new char[n_fill]).replace('\0', '0');
+  // strlong = strlong + fill_bits +
+  // Long.toString(bs.toLongArray()[bs.toLongArray().length - 1 - i], 2);
+  // }
+  // System.out.println("STRINGA BIT LUNGA = " + strlong.length());
+  // return strlong;
   // }
 
-  public int get_hash_count() {return this.hashCount.get();}
+  public int get_hash_count() {
+    return this.hashCount.get();
+  }
 
-  public int get_size() {return this.size.get();}
+  public int get_size() {
+    return this.size.get();
+  }
 
-  public BitSet get_bitset() {return this.bitset;}
+  public BitSet get_bitset() {
+    return this.bitset;
+  }
 
-  public BytesWritable gBytesWritable() {return this.bytes;}
+  public BytesWritable gBytesWritable() {
+    return this.bytes;
+  }
 
 }
