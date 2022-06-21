9ccfe3ef8931cd10fbd5a01519c2ba4b5ad2bac3
Marco Bellia <ing.bellia@gmail.com>
2022-06-21 16:31:24 +0200

    alternative solution

--- hadoop/bloomfilter/deploy.sh
+++ hadoop/bloomfilter/deploy.sh
@@ -4,6 +4,6 @@ hadoop fs -rm -r output_2
 hadoop fs -rm -r output_3
 mvn clean package
 cd target
-hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main title.ratings.txt output
+hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main2 title.ratings.txt output
 hadoop fs -cat output/part-r-00000
 hadoop fs -cat output_3/part-r-00000
