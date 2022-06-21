f75144cc6fb117705a3c628c374d5cf59e101cd3
MarcoRalli <marco_ralli@outlook.com>
2022-06-22 01:40:18 +0200

    super-mega test4

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper.java
@@ -22,30 +22,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import it.unipi.hadoop.BloomFilter;
 
-public class TestMapper  extends Mapper<Object, Text, BloomFilter, Text> {
+public class TestMapper  extends Mapper<Object, Text, IntWritable, Text> {
 
-    private HashMap<String, BloomFilter> bloomFilter_param = new HashMap<String, BloomFilter>();
-    //private Map<String, Integer> bloomFP = new HashMap<String, Integer>();
-    private String res = new String();
-
-    public void setup(Context context) throws IOException, InterruptedException {
-      
-      try {
-            Path pt = new Path("hdfs://hadoop-namenode:9820/user/hadoop/output_2/part-r-00000");// Location of file in HDFS
-            SequenceFile.Reader reader = new SequenceFile.Reader(new Configuration(), Reader.file(pt));
-            boolean hasNext;
-            do {
-
-              Text key = new Text();
-              BloomFilter bf = new BloomFilter();
-              hasNext = reader.next(key, bf);
-              bloomFilter_param.put(key.toString(), bf);
-
-            } while(hasNext);
-
-        } catch (Exception e) { e.getStackTrace(); }
-    }
- 
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
@@ -56,20 +34,22 @@ public class TestMapper  extends Mapper<Object, Text, BloomFilter, Text> {
           String ratingRaw = itr.nextToken().toString();
           String movieId = ratingRaw.split("\t")[0];
           rating = Math.round(Float.parseFloat(ratingRaw.split("\t")[1]));
-          res = (Integer.toString(rating)) + "\t" + movieId;
-          context.write(bloomFilter_param.get(rating.toString()), new Text(res));
-          /* 
           
-          for (Map.Entry<Text, BloomFilter> entry : bloomFilter_param.entrySet()) {
-            if((entry.getKey() == new Text(rating.toString())) && entry.getValue().check(movieId))
+          for(int i = 1; i < 11; i++)
+            if(i != rating)
+              context.write(new IntWritable(i), new Text(movieId));
+          /* 
+          for (Map.Entry<String, BloomFilter> entry : bloomFilter_param.entrySet()) {
+            if((entry.getKey() != rating.toString()))
 
               if(bloomFP.containsKey(rating.toString()))
              
-                bloomFP.put(rating.toString(), bloomFP.get(rating.toString())+1);      
+                bloomFP.get(rating.toString()).add(movieId);      
            
               else 
 
-                bloomFP.put(rating.toString(), 1);
+                bloomFP.put(rating.toString(), null);
+                bloomFP.get(rating.toString()).add(movieId);
             
           }*/
         
@@ -78,12 +58,14 @@ public class TestMapper  extends Mapper<Object, Text, BloomFilter, Text> {
       }
       /* 
       public void cleanup(Context context) throws IOException, InterruptedException {
-        Iterator<Map.Entry<String, Integer>> temp = bloomFP.entrySet().iterator();
-  
+        Iterator<Map.Entry<String, ArrayList<String>>> temp = bloomFP.entrySet().iterator();
+        int falsePositive;
         while(temp.hasNext()) {
-            Map.Entry<String, Integer> entry = temp.next();
+            Map.Entry<String, ArrayList<String>> entry = temp.next();
             String keyVal = entry.getKey();
-            Integer falsePositive = entry.getValue();
+            ArrayList<String> id = entry.getValue();
+            for(int i = 0; i < id.size(); i++)
+
           
             
         }
