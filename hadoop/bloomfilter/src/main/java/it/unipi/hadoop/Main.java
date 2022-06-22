74c0ad2e3154376ec21236e3cfe58ec96d1a3e3e
MarcoRalli <marco_ralli@outlook.com>
2022-06-22 11:19:40 +0200

    super-mega test17

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
@@ -23,12 +23,8 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
     
-    Configuration conf1 = new Configuration();
-    String[] otherArgs = new GenericOptionsParser(conf1, args).getRemainingArgs();
-    if (otherArgs.length < 2) {
-      System.err.println("Usage: bloom filter <in> [<in>...] <out>");
-      System.exit(2);
-    }
+    //Configuration conf1 = new Configuration();
+    
     /* 
     Job job1 = Job.getInstance(conf1, "tokenizer of data");
     job1.setInputFormatClass(NLineInputFormat.class);
@@ -80,6 +76,11 @@ public class Main {
     }
     */
     Configuration conf3 = new Configuration();
+    String[] otherArgs = new GenericOptionsParser(conf3, args).getRemainingArgs();
+    if (otherArgs.length < 2) {
+      System.err.println("Usage: bloom filter <in> [<in>...] <out>");
+      System.exit(2);
+    }
     Job job3 = Job.getInstance(conf3, "bloom filter creator");
     job3.setInputFormatClass(NLineInputFormat.class);
     NLineInputFormat.addInputPath(job3, new Path(args[0]));
