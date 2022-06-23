e536922d0e13123c6fc785a54b9c6a89b4691d5b
MarcoRalli <marco_ralli@outlook.com>
2022-06-23 18:02:21 +0200

    spark hadoop7

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/BloomFilterUtility.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/BloomFilterUtility.java
@@ -1,5 +1,13 @@
 package it.unipi.hadoop;
 
+import org.apache.hadoop.fs.FileSystem;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+
 public class BloomFilterUtility {
 
   public static int get_size(int n, Double p) {
@@ -11,4 +19,27 @@ public class BloomFilterUtility {
     return (int) ((size / n) * Math.log(2));
   }
   
+  public static Double countFalsePositiveRate() {
+    
+    Double fp = 0.0;
+    try {
+      Path pt = new Path("hdfs://hadoop-namenode:9820/user/hadoop_3.1/output/part-r-00000");// Location of file in HDFS
+      FileSystem fs = FileSystem.get(new Configuration());
+      BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
+      String line;
+    
+      line = br.readLine();
+      
+      while (line != null) {
+        String[] currencies = line.split("\t");
+        fp += Double.parseDouble(currencies[1]);
+        line = br.readLine();
+      }
+   
+
+    } catch (Exception e) { e.getStackTrace(); }
+
+    return fp;
+
+  }
 }
--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
@@ -1,9 +1,11 @@
 package it.unipi.hadoop;
 
 
+import java.io.IOException;
 import java.util.HashMap;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.web.resources.NewLengthParam;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -21,13 +23,12 @@ import it.unipi.hadoop.Job3.TestMapper2;
 import it.unipi.hadoop.Job3.TestReducer1;
 import it.unipi.hadoop.Job3.TestReducer2;
 
-import org.apache.hadoop.fs.FileSystem;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
 
 public class Main {
 
   public static HashMap<Text, BloomFilter> bloomFilter_param = new HashMap<Text, BloomFilter>();
+  private static long startTime;
+  private static long stopTime;
 
   public static void main(String[] args) throws Exception {
     
@@ -37,61 +38,36 @@ public class Main {
       System.err.println("Usage: bloom filter <in> [<in>...] <out>");
       System.exit(2);
     }
-    
-    Job job1 = Job.getInstance(conf1, "tokenizer of data");
-    job1.setInputFormatClass(NLineInputFormat.class);
-    NLineInputFormat.addInputPath(job1, new Path(args[0]));
-    job1.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", Integer.parseInt(otherArgs[2]));
-    job1.getConfiguration().setDouble("mapreduce.input.p_rate", Double.parseDouble(otherArgs[3]));
-    job1.setJarByClass(Main.class);
-    job1.setMapperClass(RatingMapper.class);
-    job1.setCombinerClass(Job1Combiner.class);
-    job1.setReducerClass(CreateParametersReducer.class);
 
-    job1.setMapOutputKeyClass(Text.class);
-    job1.setMapOutputValueClass(IntWritable.class); // set output values for mapper
-    
-    job1.setOutputKeyClass(Text.class);
-    job1.setOutputValueClass(Text.class);
+    startTime= System.nanoTime();
+    Job1(conf1, otherArgs);
+    stopTime = System.nanoTime();
+    System.out.println("TEMPO DI ESECUZIONE JOB1:" + (stopTime - startTime));
    
-    FileOutputFormat.setOutputPath(job1,
-        new Path(otherArgs[1]));
-    Boolean countSuccess = job1.waitForCompletion(true);
-    if(!countSuccess) { 
-      System.exit(0);
-    }
-    
-    
-    Configuration conf2 = new Configuration();
-    Job job2 = Job.getInstance(conf2, "test");
-    job2.setInputFormatClass(NLineInputFormat.class);
-    NLineInputFormat.addInputPath(job2, new Path(args[0]));
-    
-    job2.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", Integer.parseInt(otherArgs[2]));
-    job2.getConfiguration().setDouble("mapreduce.input.p_rate", Double.parseDouble(otherArgs[3]));
-    job2.setJarByClass(Main.class);
-    job2.setMapperClass(BloomFiltersMapper.class);
-    job2.setCombinerClass(BloomFiltersReducer.class);
-    job2.setReducerClass(BloomFiltersReducer.class);
+    startTime= System.nanoTime();
+    Job2(otherArgs);
+    stopTime = System.nanoTime();
+    System.out.println("TEMPO DI ESECUZIONE JOB2:" + (stopTime - startTime));
 
-    job2.setMapOutputKeyClass(Text.class);
-    job2.setMapOutputValueClass(BloomFilter.class); 
+    startTime= System.nanoTime();
+    Job3(otherArgs);
+    stopTime = System.nanoTime();
+    System.out.println("TEMPO DI ESECUZIONE JOB3:" + (stopTime - startTime));
     
-    job2.setOutputKeyClass(Text.class);
-    job2.setOutputValueClass(BloomFilter.class);
+    System.out.println("FALSE POSITIVE RATE:" + (BloomFilterUtility.countFalsePositiveRate()/10));
+     
+    System.exit(0);
+
     
-    FileOutputFormat.setOutputPath(job2, new Path(otherArgs[1] + "_2"));
-    job2.setOutputFormatClass(SequenceFileOutputFormat.class);
-    Boolean countSuccess2 = job2.waitForCompletion(true);
-    if(!countSuccess2) {
-      System.exit(0);
-    }
-    
+  }
+
+  private static void Job3(String[] otherArgs) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException{
+
     String outputTempDir = otherArgs[1] + "_3";
     Configuration conf3 = new Configuration();
     Job job3 = Job.getInstance(conf3, "bloom filter creator");
     job3.setInputFormatClass(NLineInputFormat.class);
-    NLineInputFormat.addInputPath(job3, new Path(args[0]));
+    NLineInputFormat.addInputPath(job3, new Path(otherArgs[0]));
     job3.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", Integer.parseInt(otherArgs[2])/20);
     job3.setJarByClass(Main.class);
     job3.setMapperClass(TestMapper1.class);
@@ -125,36 +101,66 @@ public class Main {
       }
     }
     
-    System.out.println("FALSE POSITIVE RATE:" + (countFalsePositiveRate()/10));
-     
-    System.exit(0);
-
-    
   }
 
-  private static Double countFalsePositiveRate() {
+  private static void Job1(Configuration conf1, String[] otherArgs) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException {
+
+    Job job1 = Job.getInstance(conf1, "tokenizer of data");
+    job1.setInputFormatClass(NLineInputFormat.class);
+    NLineInputFormat.addInputPath(job1, new Path(otherArgs[0]));
+    job1.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", Integer.parseInt(otherArgs[2]));
+    job1.getConfiguration().setDouble("mapreduce.input.p_rate", Double.parseDouble(otherArgs[3]));
+    job1.setJarByClass(Main.class);
+    job1.setMapperClass(RatingMapper.class);
+    job1.setCombinerClass(Job1Combiner.class);
+    job1.setReducerClass(CreateParametersReducer.class);
+
+    job1.setMapOutputKeyClass(Text.class);
+    job1.setMapOutputValueClass(IntWritable.class); // set output values for mapper
     
-    Double fp = 0.0;
-    try {
-      Path pt = new Path("hdfs://hadoop-namenode:9820/user/hadoop_3.1/output/part-r-00000");// Location of file in HDFS
-      FileSystem fs = FileSystem.get(new Configuration());
-      BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
-      String line;
-    
-      line = br.readLine();
-      
-      while (line != null) {
-        String[] currencies = line.split("\t");
-        fp += Double.parseDouble(currencies[1]);
-        line = br.readLine();
-      }
+    job1.setOutputKeyClass(Text.class);
+    job1.setOutputValueClass(Text.class);
+   
+    FileOutputFormat.setOutputPath(job1,
+        new Path(otherArgs[1]));
+    Boolean countSuccess = job1.waitForCompletion(true);
+    if(!countSuccess) { 
+      System.exit(0);
+    }
    
 
-    } catch (Exception e) { e.getStackTrace(); }
-
-    return fp;
-
   }
+
+  private static void Job2(String[] otherArgs) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException {
+
+    Configuration conf2 = new Configuration();
+    Job job2 = Job.getInstance(conf2, "test");
+    job2.setInputFormatClass(NLineInputFormat.class);
+    NLineInputFormat.addInputPath(job2, new Path(otherArgs[0]));
+    
+    job2.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", Integer.parseInt(otherArgs[2]));
+    job2.getConfiguration().setDouble("mapreduce.input.p_rate", Double.parseDouble(otherArgs[3]));
+    job2.setJarByClass(Main.class);
+    job2.setMapperClass(BloomFiltersMapper.class);
+    job2.setCombinerClass(BloomFiltersReducer.class);
+    job2.setReducerClass(BloomFiltersReducer.class);
+
+    job2.setMapOutputKeyClass(Text.class);
+    job2.setMapOutputValueClass(BloomFilter.class); 
+    
+    job2.setOutputKeyClass(Text.class);
+    job2.setOutputValueClass(BloomFilter.class);
+    
+    FileOutputFormat.setOutputPath(job2, new Path(otherArgs[1] + "_2"));
+    job2.setOutputFormatClass(SequenceFileOutputFormat.class);
+    Boolean countSuccess2 = job2.waitForCompletion(true);
+    if(!countSuccess2) {
+      System.exit(0);
+    }
+    
+  }
+
+  
 
 }
 
