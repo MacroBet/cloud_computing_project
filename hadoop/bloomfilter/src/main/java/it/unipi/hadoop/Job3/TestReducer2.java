308c7e2a9568a45b33a19fa76de2e4abf6ce2614
MarcoRalli <marco_ralli@outlook.com>
2022-06-23 21:16:28 +0200

    fix hadoop3

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestReducer2.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestReducer2.java
@@ -2,11 +2,10 @@ package it.unipi.hadoop.Job3;
 
 
 import java.io.IOException;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class TestReducer2 extends Reducer<Text, Text, Text, DoubleWritable> {
+public class TestReducer2 extends Reducer<Text, Text, Text, Text> {
 
   public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
@@ -18,6 +17,6 @@ public class TestReducer2 extends Reducer<Text, Text, Text, DoubleWritable> {
         n++;
     }
 
-    context.write(key, new DoubleWritable(falsePositive/n));
+    context.write(key, new Text(String.valueOf(falsePositive/n)));
   }
 }
