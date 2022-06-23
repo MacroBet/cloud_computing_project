1c932b78bb2eaa49c2305e424fb7c8e0625abce2
MarcoRalli <marco_ralli@outlook.com>
2022-06-23 19:19:02 +0200

    spark hadoop10

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job1/CreateParametersReducer.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job1/CreateParametersReducer.java
@@ -17,7 +17,7 @@ public class CreateParametersReducer extends Reducer<Text, IntWritable, Text, Te
         sum += val.get();
        
       }
-      int m = BloomFilterUtility.get_size(sum, context.getConfiguration().getDouble("mapreduce.input.p_rate", 0.1));
+      int m = BloomFilterUtility.get_size(sum, 0.1);//context.getConfiguration().getDouble("mapreduce.input.p_rate", 0.1));
       int k = BloomFilterUtility.get_hash_count(m, sum);
 
       res = (Integer.toString(m) + "\t" + Integer.toString(k) + "\t" + Integer.toString(sum));
