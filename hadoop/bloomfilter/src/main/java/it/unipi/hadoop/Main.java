5df9c5a348ba7be598a5dcc21b9c25409bb4cf5f
MarcoRalli <marco_ralli@outlook.com>
2022-06-24 23:19:35 +0200

    fix hadoop57

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
@@ -95,7 +95,7 @@ public class Main {
       FileOutputFormat.setOutputPath(job3_1, new Path(args[1] + "_3.1"));
       Boolean countSuccess3_1 = job3_1.waitForCompletion(true);
       if(countSuccess3_1) {
-        Job job3_2 = Job.getInstance(conf3, "JOB_3.1");
+      /*Job job3_2 = Job.getInstance(conf3, "JOB_3.1");
       job3_2.setJarByClass(Main.class);
       job3_2.setMapperClass(TestMapper3.class);
       job3_2.setReducerClass(TestReducer3.class);
@@ -109,11 +109,11 @@ public class Main {
       NLineInputFormat.addInputPath(job3_2, new Path(outputTempDir+ "/part-r-00001"));
       FileOutputFormat.setOutputPath(job3_2, new Path(args[1] + "_3.2"));
       Boolean countSuccess3_32 = job3_2.waitForCompletion(true);
-      if(!countSuccess3_32) {
+      if(!countSuccess3_32) {*/
         System.exit(0);
       }
       }
-    }
+    //}
     
     
     stopTime = System.currentTimeMillis();
