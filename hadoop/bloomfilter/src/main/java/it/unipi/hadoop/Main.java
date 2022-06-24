19a4362e212ac783e0588529c269ca8c63c44a13
MarcoRalli <marco_ralli@outlook.com>
2022-06-24 19:29:32 +0200

    fix hadoop53

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
@@ -82,8 +82,8 @@ public class Main {
     if(countSuccess3) {
       Job job3_1 = Job.getInstance(conf3, "JOB_3.1");
       job3_1.setJarByClass(Main.class);
-      job3_1.setMapperClass(TestMapper2.class);
-      job3_1.setReducerClass(TestReducer2.class);
+      job3_1.setMapperClass(TestMapper3.class);
+      job3_1.setReducerClass(TestReducer3.class);
       job3_1.setInputFormatClass(NLineInputFormat.class);
       job3_1.getConfiguration().setInt("mapreduce.input.lineinputformalinespermapt.", (N_SPLIT));
       job3_1.setMapOutputKeyClass(Text.class);
