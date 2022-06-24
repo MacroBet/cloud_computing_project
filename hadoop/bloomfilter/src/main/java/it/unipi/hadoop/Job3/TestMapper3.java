d5ce0f5e2850e3317c66bf0bbf7d0fba4e0d48ac
MarcoRalli <marco_ralli@outlook.com>
2022-06-24 16:47:15 +0200

    fix hadoop33

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper3.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper3.java
@@ -16,6 +16,26 @@ import org.apache.hadoop.mapreduce.Mapper;
 import it.unipi.hadoop.BloomFilter;
 
 public class TestMapper3 extends Mapper<Object, Text, Text,Text> {
+    private HashMap<Text, BloomFilter> bloomFilter_param = new HashMap<Text, BloomFilter>();
+    private Map<String, String> combiner = new HashMap<String, String>(); 
+
+    public void setup(Context context) throws IOException, InterruptedException {
+      
+      try {
+            Path pt = new Path("hdfs://hadoop-namenode:9820/user/hadoop/output_2/part-r-00000");// Location of file in HDFS
+            SequenceFile.Reader reader = new SequenceFile.Reader(new Configuration(), Reader.file(pt));
+            boolean hasNext;
+            do {
+  
+              Text key = new Text();
+              BloomFilter bf = new BloomFilter();
+              hasNext = reader.next(key, bf);
+              bloomFilter_param.put(key, bf);
+  
+            } while(hasNext);
+  
+        } catch (Exception e) { e.getStackTrace(); }
+    }
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
  
         StringTokenizer itr = new StringTokenizer(value.toString(), "\n");
@@ -24,7 +44,8 @@ public class TestMapper3 extends Mapper<Object, Text, Text,Text> {
           String ratingRaw = itr.nextToken().toString();
           int rating = Integer.parseInt(ratingRaw.split("\t")[0]);
           String id = ratingRaw.split("\t")[1];
-          String param = String.valueOf(rating) + id;
+          //combiner.put(String.valueOf(rating), id);      
+
           context.write(new Text(String.valueOf(rating)), new Text(id));
 
           /*if( rating >1 || rating < 4)
@@ -38,4 +59,20 @@ public class TestMapper3 extends Mapper<Object, Text, Text,Text> {
             context.write(new Text("group3"), new Text(param));*/
         }
     }
+    /* 
+    public void cleanup(Context context) throws IOException, InterruptedException {
+       
+        Iterator<Map.Entry<String, ArrayList<Integer>>> temp = combiner.entrySet().iterator();
+      
+        while(temp.hasNext()) {
+            Map.Entry<String, ArrayList<Integer>> entry = temp.next();
+            String keyVal = entry.getKey();
+            Double falsePositive = (double) entry.getValue().get(0);
+            Double NonFalsePositive = (double) entry.getValue().get(1);
+            Double FPrate = falsePositive/(falsePositive+NonFalsePositive);
+      
+            context.write(new Text(keyVal), new Text(String.valueOf(FPrate)));
+        }
+          
+      }*/
 }
