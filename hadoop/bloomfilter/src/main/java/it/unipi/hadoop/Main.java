d2f703f5ce5aca51c92059098f5d740da368b7f3
MarcoRalli <marco_ralli@outlook.com>
2022-06-24 17:35:41 +0200

    fix hadoop36

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Main.java
@@ -84,11 +84,11 @@ public class Main {
       String outputTempDir2 = args[1] + "_3.1";
       Job job3_1 = Job.getInstance(conf3, "JOB_3.1");
       job3_1.setJarByClass(Main.class);
-      job3_1.setMapperClass(TestMapper21.class);
-      job3_1.setReducerClass(TestCombiner2.class);
+      job3_1.setMapperClass(TestMapper2.class);
+      job3_1.setReducerClass(TestReducer2.class);
       job3_1.setInputFormatClass(NLineInputFormat.class);
       //job3_1.setCombinerClass(TestCombiner3.class);
-      job3_1.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", 1000000);
+      job3_1.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", 90000);
       job3_1.setMapOutputKeyClass(Text.class);
       job3_1.setMapOutputValueClass(Text.class); 
       job3_1.setOutputKeyClass(Text.class);
