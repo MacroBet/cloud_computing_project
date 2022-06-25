a298de2cff0dac1cbbc3eb049fc915aa0c4c6fa1
MarcoRalli <marco_ralli@outlook.com>
2022-06-25 11:08:07 +0200

    fix hadoop69

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper2.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper2.java
@@ -47,7 +47,7 @@ public class TestMapper2  extends Mapper<Object, Text, Text,Text> {
           String ratingRaw = itr.nextToken().toString();
           int rating = Integer.parseInt(ratingRaw.split("\t")[0]);
           String id = ratingRaw.split("\t")[1];
-          
+            
           if(bloomFilter_param.get(new Text(String.valueOf(rating))).check(id)) {
          
             if(combiner.containsKey(String.valueOf(rating))) { 
--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper21.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper21.java
@@ -49,13 +49,13 @@ public class TestMapper21  extends Mapper<Object, Text, Text,Text>{
           String ratingRaw = itr.nextToken().toString();
           int rating = Integer.parseInt(ratingRaw.split("\t")[0]);
           String id = ratingRaw.split("\t")[1];
-          
-          if(bloomFilter_param.get(new Text(String.valueOf(rating))).check(id)) 
-            context.write(new Text(String.valueOf(rating)), new Text("1"));
-          else
-            context.write(new Text(String.valueOf(rating)), new Text("0"));
-          if(n == line/2)
-            break;
+          if(id.equals("0") || id.equals("1"))
+            context.write(new Text(String.valueOf(rating)), new Text(id));
+
+          else if(bloomFilter_param.get(new Text(String.valueOf(rating))).check(id)) 
+              context.write(new Text(String.valueOf(rating)), new Text("1"));
+            else
+              context.write(new Text(String.valueOf(rating)), new Text("0"));
         
         }
       }
