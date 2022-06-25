be5e4544f1e9d0e0f4ae60bbb7d3eacd773f4ccb
MarcoRalli <marco_ralli@outlook.com>
2022-06-25 13:40:47 +0200

    fix hadoop 94

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestReducer1.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestReducer1.java
@@ -9,20 +9,17 @@ public class TestReducer1 extends Reducer<Text, Text, Text, Text> {
   public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
     double fp = 0.0;
     int n = 0;
-    String s = "not Now";
     for (Text val : values) {
       if(val.toString().equals("1")) {
         n++;
         fp++;
       }else if(val.toString().equals("0"))
         n++;
-      else
-        s = val.toString();
+      
     }
     if(n != 0)
       context.write(key, new Text(String.valueOf(fp/n)));
-    else
-      context.write(key, new Text(s));
+  
 
 
   }
