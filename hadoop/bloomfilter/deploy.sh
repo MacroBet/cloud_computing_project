6856c0339c12fce00e5dea06f450eb6696056209
MarcoRalli <marco_ralli@outlook.com>
2022-06-25 15:45:12 +0200

    test phase11

--- hadoop/bloomfilter/deploy.sh
+++ hadoop/bloomfilter/deploy.sh
@@ -1,7 +1,5 @@
 git pull
-hadoop fs -rm -r output
-hadoop fs -rm -r output_2
-hadoop fs -rm -r output_3
+
 hadoop fs -rm -r output_3.1
 hadoop fs -rm -r output_3.2
 hadoop fs -rm -r output_3.3
@@ -14,7 +12,6 @@ hadoop fs -rm -r output_3.9
 hadoop fs -rm -r output_3.10
 mvn clean package
 cd target
-hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main title.ratings.txt output 100000 0.01 
 hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Test title.ratings.txt output 100000
 hadoop fs -cat output/part-r-00000
 hadoop fs -cat output_3.1/part-r-00000
