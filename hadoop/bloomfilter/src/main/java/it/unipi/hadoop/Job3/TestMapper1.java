79a100de3e90d28f71f37e1d7fa7a32f6bf154bf
MarcoRalli <marco_ralli@outlook.com>
2022-06-25 11:39:06 +0200

    fix hadoop76

--- hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper1.java
+++ hadoop/bloomfilter/src/main/java/it/unipi/hadoop/Job3/TestMapper1.java
@@ -46,12 +46,14 @@ public class TestMapper1  extends Mapper<Object, Text, Text,Text> {
           String ratingRaw = itr.nextToken().toString();
           String movieId = ratingRaw.split("\t")[0];
           rating = Math.round(Float.parseFloat(ratingRaw.split("\t")[1]));
-          for(int i = 1; i < 11; i++)
-            if(i != rating && rating < 4)
-              if(bloomFilter_param.get(new Text(String.valueOf(rating))).check(movieId)) 
+          for(int i = 1; i < 11; i++){
+            if(i != rating && rating < 4){
+              if(bloomFilter_param.get(new Text(String.valueOf(rating))).check(movieId)) {
                   context.write(new Text(String.valueOf(rating)), new Text("1"));
-                else
+              }else
                   context.write(new Text(String.valueOf(rating)), new Text("0"));
+            }
+          }
            // }else      
              //   context.write(new Text(String.valueOf(i)), new Text(movieId));  
           /* 
