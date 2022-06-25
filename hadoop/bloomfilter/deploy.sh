adca99cacb0a8c273185aab2142ed740f2fb23f5
MarcoRalli <marco_ralli@outlook.com>
2022-06-25 15:10:18 +0200

    test phase8

--- hadoop/bloomfilter/deploy.sh
+++ hadoop/bloomfilter/deploy.sh
@@ -14,7 +14,7 @@ hadoop fs -rm -r output_3.9
 hadoop fs -rm -r output_3.10
 mvn clean package
 cd target
-hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main mini_ratings.txt output 100000 0.01 
+hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main title.ratings.txt output 100000 0.01 
 hadoop fs -cat output/part-r-00000
 hadoop fs -cat output_3.1/part-r-00000
 hadoop fs -cat output_3.2/part-r-00000
