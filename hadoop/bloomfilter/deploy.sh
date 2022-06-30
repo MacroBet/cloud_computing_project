2a5b1a5b21e5e78056584e133712ba43e7eb9764
Marco Bellia <ing.bellia@gmail.com>
2022-06-30 17:41:44 +0200

    deploy test

--- hadoop/bloomfilter/deploy.sh
+++ hadoop/bloomfilter/deploy.sh
@@ -2,10 +2,9 @@ git pull
 hadoop fs -rm -r output
 hadoop fs -rm -r output_2
 hadoop fs -rm -r output_3
-hadoop fs -rm -r output_3.1
 mvn clean package
 cd target
-hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main title.ratings.txt output 100000 0.01 
+hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main mini_ratings.txt output 100000 0.01 
 hadoop fs -cat output/part-r-00000
-hadoop fs -cat output_3.1/part-r-00000
+hadoop fs -cat output_3/part-r-00000
 
