e68e40cf9c05b9d4d71b66679ddb213dfaf702a5
Stefano Bianchettin <s.bianchettin@studenti.unipi.it>
2022-07-04 18:10:08 +0200

    test

--- hadoop/bloomfilter/deploy.sh
+++ hadoop/bloomfilter/deploy.sh
@@ -4,7 +4,7 @@ hadoop fs -rm -r output_2
 hadoop fs -rm -r output_3
 mvn clean package
 cd target
-hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main title.ratings.txt output 150000 0.1 
+hadoop jar bloomfilter-1.0-SNAPSHOT.jar it.unipi.hadoop.Main title.ratings.txt output 150000 0.001 
 hadoop fs -cat output/part-r-00000
 hadoop fs -cat output_3/part-r-00000
 
